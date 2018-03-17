8f0ff516cc0a4d93ece08b378397aee8b49e5490
Alex Lee <al8876@gmail.com>
2018-03-16 23:06:38 -0700

    Push button plays wow sound

--- wowi/App.js
+++ wowi/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Button } from 'react-native';
 import { Constants, WebBrowser, LinearGradient } from 'expo';
 
 
 export default class App extends React.Component {
+  
+  Play() {
+    this.clickSound();
+  }
+    
+  async clickSound() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow1.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
 
         <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
        
-       <Text style={{color: 'white', fontSize: 20 }}>CLICK TO WOW</Text>
+       <Text style={{color: 'white', fontSize: 20 }}>CLICK TO WOW{"\n"}{"\n"}</Text>
 
-        <TouchableOpacity onPress={this._openWebBrowserAsync}> 
+        <TouchableOpacity onPress={() => this.Play()}> 
           <Image style={{ width: 200, height: 200 }}source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Perspective-Button-Stop-icon.png'}} />
+          <Image style={{ position: 'absolute', width: 100, height: 100, marginLeft: 50, marginTop: 25 }}source={{ uri: 'https://cdn.famousoutfits.com/wp-content/uploads/2014/12/owen-wilson.png'}} />
         </TouchableOpacity>
 
       </View>
