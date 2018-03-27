8d2298f1848a4491591c6d580f422c72f7f54435
Alex Lee <al8876@gmail.com>
2018-03-27 12:54:55 -0700

    Randomized sounds between 6 mp3s

--- wowi/App.js
+++ wowi/App.js
@@ -10,15 +10,82 @@ const { width, height } = Dimensions.get('window');
 
 
 export default class App extends React.Component {
+
+
   
   Play() {
-    this.clickSound();
+
+    let getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    let num = getRandomInt(1, 6);
+
+    if (num === 1) {
+      this.clickSound();    
+    } else if (num === 2) {
+      this.clickSound2();
+    } else if (num === 3) {
+      this.clickSound3();
+    } else if (num === 4) {
+      this.clickSound4();
+    } else if (num === 5) {
+      this.clickSound5();
+    } else if (num === 6) {
+      this.clickSound6();
+    }
+
   }
     
   async clickSound() {
     const soundObject = new Expo.Audio.Sound();
       try {
         await soundObject.loadAsync(require('./assets/wow1.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
+  async clickSound2() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow2.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
+  async clickSound3() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow3.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
+  async clickSound4() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow4.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
+  async clickSound5() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow5.mp3'));
+        await soundObject.playAsync();
+      } catch (error) {
+    }
+  }
+
+  async clickSound6() {
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('./assets/wow6.mp3'));
         await soundObject.playAsync();
       } catch (error) {
     }
